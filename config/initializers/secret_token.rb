9c13f4fc6c59b197d29fac9701f5339966a62f9e
Fatih AYTAÇ <fatih.aytac@gmail.com>
2012-11-28 02:21:03 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anahaber::Application.config.secret_token = '7367200bc82bfedda431755bdc9f08c73aa3a3247431b11b35bfbdc7993f279216da9ef7bc425aba5e1a13609bf6b61001229dc947454cbfd9e6efae805bafde'
