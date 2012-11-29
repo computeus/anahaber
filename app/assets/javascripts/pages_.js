383c9e53e2abf6640bf9dc539a32be866a1404e8
Fatih AYTAÇ <fatih.aytac@gmail.com>
2012-11-29 23:40:30 +0200

    RSS read, extracted and data and labels arrays formed with the related datas.

--- app/assets/javascripts/pages_.js
+++ app/assets/javascripts/pages_.js
@@ -1,20 +1,27 @@
 $(document).ready(function () {
-    data = [[60000, 60000, 40000, 30000, 20000, 10000], [60000, 60000, 40000, 30000, 20000, 10000], [60000, 60000, 40000, 30000, 20000, 10000], [60000, 60000, 40000, 30000, 20000, 10000]];
-    labels = [["Paris", "London", "New York", "Moscow", "Berlin", "Tokyo"], ["Paris", "London", "New York", "Moscow", "Berlin", "Tokyo"], ["Paris", "London", "New York", "Moscow", "Berlin", "Tokyo"], ["Paris", "London", "New York", "Moscow", "Berlin", "Tokyo"]];
+    data = new Array();
+    labels = new Array();
 
-    Treemap.draw("container-first", $("#container").width(), $(window).height() - 100, data, labels);
+    $.ajax({
+        type:"GET",
+        async:false,
+        url:"/pages/get_news?kind=world",
+        dataType:"xml",
+        success:function (xml) {
+            $(xml).find("item").each(function () {
+                data.push(parseInt($(this).find("description").text().replace(/(.*?)(\d+)( haber makalesinin .*)/, "$2")));
+                labels.push($(this).find("title").text());
+            });
+        }
+    });
 
-//    $('#container-first').isotope({
-//        itemSelector: '.element',
-//        resizable: false,
-//        masonry: { columnWidth: $('#container-first').width() / 4 }
-//    });
-//
-    $(window).smartresize(function(){
-//        $('#container-first').isotope({
-//            masonry: { columnWidth: $('#container-first').width() / 4 }
-//        });
+
+    Treemap.draw("container-first", $("#container").width(), $(window).height() - 100, data, labels, {'label':{'font-size':'16px'}});
+
+    $(window).smartresize(function () {
         $("#container-first").html('');
         Treemap.draw("container-first", $("#container").width(), $(window).height() - 100, data, labels);
     });
+
+
 });
